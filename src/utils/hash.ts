48aa8ba4234e829f1d233118f94321bc4132ab1b
Antonio Hauren <antoniohauren@protonmail.com>
2024-03-13 14:31:07 -0300

    chore(utils):  add hash

---
+++ src/utils/hash.ts
@@ -0,0 +1,15 @@
+import crypto from "crypto";
+
+export function generateSalt() {
+  return crypto.randomBytes(16).toString("hex");
+}
+
+export function generateHash(input: string, salt: string) {
+  return crypto.pbkdf2Sync(input, salt, 100, 64, "sha512").toString("hex");
+}
+
+export function isValidHash(input: string, salt: string, hash: string) {
+  const hashed = generateHash(input, salt);
+
+  return hashed === hash;
+}

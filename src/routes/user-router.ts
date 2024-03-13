11880def0f97e37e7f0fbc6f2789b57660ab5342
Antonio Hauren <antoniohauren@protonmail.com>
2024-03-13 13:46:20 -0300

    chore: add email validator to insertUserSchema

--- src/routes/user-router.ts
+++ src/routes/user-router.ts
@@ -2,10 +2,15 @@ import { createUserHandler, getUserHandler } from "@/handlers";
 import { insertUserSchema } from "@/schemas";
 import { validator } from "@/utils/schema-validator";
 import { Hono } from "hono";
+import { z } from "zod";
 
 const userRouter = new Hono();
 
+const createSchema = insertUserSchema.extend({
+  email: z.string().email(),
+});
+
 userRouter.get("/:id", getUserHandler);
-userRouter.post("/create", validator(insertUserSchema), createUserHandler);
+userRouter.post("/create", validator(createSchema), createUserHandler);
 
 export { userRouter };

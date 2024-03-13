1ae9397eb1e1bb8ed09f87a145937929c928136b
Antonio Hauren <antoniohauren@protonmail.com>
2024-03-13 12:57:19 -0300

    chore(user): add schemas

---
+++ src/schemas/user-schema.ts
@@ -0,0 +1,18 @@
+import { pgTable, timestamp, uuid, varchar } from "drizzle-orm/pg-core";
+import { createInsertSchema, createSelectSchema } from "drizzle-zod";
+import { z } from "zod";
+
+const user = pgTable("user", {
+  id: uuid("id").defaultRandom().notNull().primaryKey(),
+  name: varchar("name", { length: 256 }).notNull(),
+  username: varchar("username", { length: 256 }).notNull().unique(),
+  email: varchar("email", { length: 256 }).notNull().unique(),
+  created_at: timestamp("created_at").defaultNow().notNull(),
+  updated_at: timestamp("updated_at").defaultNow().notNull(),
+});
+
+export const selectUserSchema = createSelectSchema(user);
+export const insertUserSchema = createInsertSchema(user);
+
+export type SelectUser = z.infer<typeof selectUserSchema>;
+export type InsertUser = z.infer<typeof insertUserSchema>;

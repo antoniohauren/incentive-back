5d5fa7a9db68f285b70589f10aadfdb23dff05b2
Antonio Hauren <antoniohauren@protonmail.com>
2024-03-13 17:24:44 -0300

    chore: jwt middleware

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,20 @@
+import "dotenv/config";
+
 import { authRouter, helloRouter, userRouter } from "@/routes";
 import { Hono } from "hono";
+import { jwt } from "hono/jwt";
 
 const app = new Hono();
 
+// PUBLIC ROUTES
 app.route("/", helloRouter);
-app.route("/user", userRouter);
-
 app.route("/auth", authRouter);
+
+// SETUP JWT MIDDLEWARE
+const secret = process.env.JWT_SECRET || "";
+app.use("/*", jwt({ secret }));
+
+// PRIVATE ROUTES
+app.route("/user", userRouter);
 
 export default app;

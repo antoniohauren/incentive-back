96d4536af6840db39b11a41b04f2647086a1cfe9
Antonio Hauren <antoniohauren@protonmail.com>
2024-03-13 13:39:51 -0300

    chore: add db handler

---
+++ src/drizzle/index.ts
@@ -0,0 +1,26 @@
+import "dotenv/config";
+import { Pool } from "pg";
+
+import { NodePgDatabase, drizzle } from "drizzle-orm/node-postgres";
+
+import * as schema from "@/schemas";
+
+if (
+  !process.env.DB_NAME ||
+  !process.env.DB_HOST ||
+  !process.env.DB_USER ||
+  !process.env.DB_PASS ||
+  !process.env.DB_PORT
+) {
+  throw new Error("Missing database credentials!");
+}
+
+const pool = new Pool({
+  port: Number(process.env.DB_PORT),
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+});
+
+export const db: NodePgDatabase<typeof schema> = drizzle(pool, { schema });

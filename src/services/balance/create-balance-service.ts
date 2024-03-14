d6b1dd10e019b554ba38c46b27e55391cacc6855
Antonio Hauren <antoniohauren@protonmail.com>
2024-03-13 21:34:24 -0300

    chore(balance): add service

---
+++ src/services/balance/create-balance-service.ts
@@ -0,0 +1,27 @@
+import type { BalanceRequest } from "@/models/balance-model";
+import { createBalanceRepository } from "@/repositories/balance";
+import type { InsertBalance, SelectBalance } from "@/schemas";
+import type { ServiceReturn } from "@/utils/types";
+
+export async function createBalanceService(
+  data: BalanceRequest,
+): Promise<ServiceReturn<SelectBalance>> {
+  const dto: InsertBalance = {
+    name: data.name,
+    description: data.description,
+    startMoney: data.startMoney,
+    currentMoney: data.startMoney,
+  };
+
+  const balance = await createBalanceRepository(dto);
+
+  if (!balance.success || !balance.data || !balance.data.length) {
+    return { success: false, message: "FAILED_TO_CREATE_BALANCE" };
+  }
+
+  return {
+    success: balance.success,
+    message: "BALANCE_CREATED",
+    data: balance.data[0],
+  };
+}
